417d68fad15fd2fb58aa7e9b82791f894784ceec
ibibeklamichhane <bibeklc714@gmail.com>
2023-09-14 20:11:38 +0545

    added the blogs contents

---
+++ src/components/BlogCard.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+function BlogCard({blog}) {
+  return (
+    <div className="w-1/3 p-4">
+  <div className=" shadow-outer hover:shadow-inner transition-all duration-1000 ease-in-out content-center bg-slate-200  border border-green-300 rounded-lg shadow-md overflow-hidden mt-6">
+     
+      <img src={blog.image} alt={blog.title} className="h-40 w-full object-cover" />
+      <div className="p-4">
+        <h3 className="text-xl font-semibold mb-2">{blog.title}</h3>
+        <p className="text-gray-600 mb-4">{blog.content.substring(0, 150) + '...'}</p>
+        <div className="flex items-center">
+          <img src={blog.author.avatar} alt={blog.author.name} className="h-10 w-10 rounded-full object-cover" />
+          <p className="text-gray-700 ml-2">{`Posted by ${blog.author.name}`}</p>
+        </div>
+        <div className="flex justify-between items-center">
+        <p className="text-gray-500 mt-1">{new Date(blog.date).toDateString()}</p>
+        <button className=" shadow-outer hover:shadow-inner transition-all duration-1000 ease-in-out mt-4 bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-full focus:outline-none cursor-pointer">
+              Read More
+          </button>
+          </div>
+
+      </div>
+    </div>
+    </div>
+  );
+}
+
+export default BlogCard;
---
+++ src/components/Blogs.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import BlogCard  from './BlogCard';
+import Blogitems  from './Blogitems';
+
+
+function Blogs() {
+  return (
+    <>
+    <h1>All Blogs</h1>
+    <Blogitems/>
+    
+
+    </>
+    
+  )
+}
+
+export default Blogs;
